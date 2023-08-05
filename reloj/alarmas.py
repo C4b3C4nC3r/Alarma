7e2b0413514232f2b41b536492dc3d94adfcd5a9
Livington <livingtonllopezc124@gmail.com>
2023-08-05 10:12:36 -0500

    Mejora visual tanto en el codigo como en la vistas, algunos errores son, que el frame de opciones se desplaza junto al contenido dle otro frame, el fram_option debe de quedar estatico, y el otro debe de tener una barra y jugar con 5 columnas o dos y un formato de tarjeta mas grande

--- reloj/alarmas.py
+++ reloj/alarmas.py
@@ -16,6 +16,7 @@ class RelojAlarma():
     check_boxes = []
     dir_audio = "data\d_alarm_sounds\herta singing kururing.mp3"
     dir_alarm = "data\historial\historial_alarms.json"
+    check_var = []
 
     def __init__(self):
         super().__init__()
@@ -35,18 +36,30 @@ class RelojAlarma():
         
         self.findAlarms()
         self.contenido_frame = contenido_frame
-        for alarma in self.__class__.alarms:
-            etq_hora = ttk.Label(contenido_frame, text=str(alarma["hora"])+" : "+str(alarma["hora"]) )
-            etq_nombre = ttk.Label(contenido_frame, text= alarma["nombre"])
-            etq_actividad = ttk.Label(contenido_frame,text= "activa" if alarma["activo"] else "inactiva")
+        for row, alarma in enumerate(self.__class__.alarms):
+            hora = str(alarma['hora']) + ":" + str(alarma["minuto"])
 
-            etq_hora.grid()
-            etq_nombre.grid()
-            etq_actividad.grid()
+            tarjeta = ttk.Label(
+                self.contenido_frame,
+                borderwidth=2,
+                relief="solid",
+                padding=10,
+            )
+            tarjeta.grid(row=row, column=0, sticky="ew", padx=10, pady=5)
 
-        btn_create = ttk.Button(contenido_frame,text="Nueva Alarma", command=self.windowCreateAlarm)
-        btn_create.grid()
+            
+            check_var = tk.BooleanVar(value=alarma["activo"])
+            self.__class__.check_var.append(check_var)
+            check_btn = ttk.Checkbutton(
+                tarjeta,
+                text=f"Hora: {hora}\n{alarma['nombre']}",
+                variable=check_var
+            )
+            check_btn.grid(row=0, column=0, sticky="w")
 
+
+        ttk.Button(contenido_frame,text="Nueva Alarma", command=self.windowCreateAlarm).grid()
+    #ventana para la creacion de nuevas alarmas
     def windowCreateAlarm(self):
 
         self.modal = tk.Toplevel(self.contenido_frame)
@@ -56,46 +69,52 @@ class RelojAlarma():
         #configuracion
         self.dataHoraMinuto()
         self.option()
+    # funcion que contiene todas las opciones
+    def option(self): 
         
-    def createNotifAlarms(self):
-        pass
+        self.horaMinutoNombre()
+        self.checkHorario() 
+        self.sonidoPosponer()
 
-    def getData(self):
+        ttk.Button(self.modal, text="Guardar",command=self.saveAlarm).grid(column=1, row=7)
+        ttk.Button(self.modal, text="Cancelar",command=self.modal.destroy).grid(column=2,row=7)
+        # Bloquear interacción con la ventana principal
+        self.modal.grab_set()
+    #1 Parte uno : Tiempo (hour and minute) y  Nombre
+    def horaMinutoNombre(self):
+        self.cmb_h = ttk.Combobox(self.modal,values=self.__class__.h_list,justify='center',width='12',font='Arial')
+        self.cmb_m = ttk.Combobox(self.modal,values=self.__class__.m_list,justify='center',width='12',font='Arial')
+        self.cmb_h.current(int(self.__class__.hour))
+        self.cmb_m.current(int(self.__class__.minute))
+        self.cmb_h.grid(column=1,row=1)
+        self.cmb_m.grid(column=2,row=1)
+        #entry
+        ttk.Label(self.modal,text="Nombre de Alarm").grid(column=1,row=2)
+        self.name_alarm = ttk.Entry(self.modal)
+        self.name_alarm.grid(column=2,row=2)
+    #2 check de repetir todos los dias o no, tambien que dias repetir la alarma
+    def checkHorario(self):
+         #repetir
+        self.confirm_var = tk.BooleanVar()
+        self.check_confirm = ttk.Checkbutton(self.modal,text="Quiere Repetir esta alarma", variable=self.confirm_var,command=self.activarRepetir)
+        self.check_confirm.grid(column=1,row=3)
+        #checkbox de dias de la semana
+        for indice,dia in enumerate(self.__class__.dia_de_la_semana):
+            var = tk.BooleanVar()
+            checkbox = ttk.Checkbutton(self.modal, text=dia, variable=var,command=self.activarDias)
+            checkbox.grid(column=indice ,row=4)
+            self.__class__.check_boxes.append(var)
+    #3 sonido o audio para alarmas y el tiempo si uno lo pospone
+    def sonidoPosponer(self):
+         #selector de sonido
+        ttk.Label(self.modal,text="Musica").grid(column=0,row=5)
+        ttk.Button(self.modal,text="Tono... ", command=self.seleccionSonido).grid(column=1,row=5)
+        ttk.Label(self.modal,text="Posponer en").grid(column=0,row=6)
+        self.cmb_posponer = ttk.Combobox(self.modal,values=[5,10,15,20,25,30,60],justify='center',width='12',font='Arial')
+        self.cmb_posponer.current(0)
+        self.cmb_posponer.grid(column=1,row=6) #minutes
 
-        self.hour_alarm = int(self.cmb_h.get())
-        self.minute_alarm = int(self.cmb_m.get())
-        self.nombre_alarm = self.name_alarm.get()
-        self.posponer = int(self.cmb_posponer.get())
-        self.veces = self.horarioCustom()
-        self.audio = self.__class__.dir_audio
-
-    def setData(self): 
-
-        self.cmb_h.set(self.__class__.hour)
-        self.cmb_m.set(self.__class__.minute)
-        self.cmb_posponer.set(5)
-        self.name_alarm.delete(0, tk.END)
-        #desactivar los checks
-        for var in self.__class__.check_boxes:
-            var.set(False)
-        self.confirm_var.set(False)
-
-    def horarioCustom(self):
-        return {}
-
-    def addData(self):
-        alarma = {
-            "hora":self.hour_alarm,
-            "minuto": self.minute_alarm,
-            "nombre": self.nombre_alarm,
-            "intervalo_posponer":self.posponer,
-            "veces_semana":self.veces,
-            "audio":self.audio,
-            "activo":True
-        }
-
-        self.__class__.alarms.append(alarma)
-
+    #Seccion de tratamiento de datos para guardar
     def saveAlarm(self):
         self.getData()
         self.addData()
@@ -116,6 +135,48 @@ class RelojAlarma():
         self.__class__.alarms = []
         self.setData()
 
+    def addData(self):
+        alarma = {
+            "hora":self.hour_alarm,
+            "minuto": self.minute_alarm,
+            "nombre": self.nombre_alarm,
+            "intervalo_posponer":self.posponer,
+            "veces_semana":self.veces,
+            "audio":self.audio,
+            "activo":True
+        }
+
+        self.__class__.alarms.append(alarma)
+
+    def getData(self):
+
+        self.hour_alarm = int(self.cmb_h.get())
+        self.minute_alarm = int(self.cmb_m.get())
+        self.nombre_alarm = self.name_alarm.get() if not len(self.name_alarm.get()) == 0 else "Alarma" 
+        self.posponer = int(self.cmb_posponer.get())
+        self.veces = self.horarioCustom()
+        self.audio = self.__class__.dir_audio
+
+    def setData(self): 
+
+        self.cmb_h.set(self.__class__.hour)
+        self.cmb_m.set(self.__class__.minute)
+        self.cmb_posponer.set(5)
+        self.name_alarm.delete(0, tk.END)
+        #desactivar los checks
+        for var in self.__class__.check_boxes:
+            var.set(False)
+        self.confirm_var.set(False)
+
+    def horarioCustom(self):
+        
+        datos_json = []
+        for dia, var in zip(self.dia_de_la_semana,self.check_boxes):
+            datos_json.append({dia:var.get()})
+
+        return datos_json
+   
+    #Seccion de tratameinto de datos en ejecucion
     def dataHoraMinuto(self):
         for i in range(0,25):
             self.__class__.h_list.append(i)
@@ -142,39 +203,25 @@ class RelojAlarma():
         filename = filedialog.askopenfilename(parent=self.modal,filetypes=filetypes)
         self.__class__.dir_audio = filename
 
-    def option(self): 
-        self.cmb_h = ttk.Combobox(self.modal,values=self.__class__.h_list,justify='center',width='12',font='Arial')
-        self.cmb_m = ttk.Combobox(self.modal,values=self.__class__.m_list,justify='center',width='12',font='Arial')
-        self.cmb_h.current(int(self.__class__.hour))
-        self.cmb_m.current(int(self.__class__.minute))
-        self.cmb_h.grid(column=1,row=1)
-        self.cmb_m.grid(column=2,row=1)
-        #entry
-        ttk.Label(self.modal,text="Nombre de Alarm")
-        self.name_alarm = ttk.Entry(self.modal)
-        self.name_alarm.grid(column=1,row=2)
-        
-        #repetir
-        self.confirm_var = tk.BooleanVar()
-        self.check_confirm = ttk.Checkbutton(self.modal,text="Quiere Repetir esta alarma", variable=self.confirm_var,command=self.activarRepetir)
-        self.check_confirm.grid(column=1,row=3)
-        #checkbox de dias de la semana
-        for indice,dia in enumerate(self.__class__.dia_de_la_semana):
-            var = tk.BooleanVar()
-            checkbox = ttk.Checkbutton(self.modal, text=dia, variable=var,command=self.activarDias)
-            checkbox.grid(column=indice ,row=4)
-            self.__class__.check_boxes.append(var)
-        #selector de sonido
-        ttk.Label(self.modal,text="Musica").grid(column=0,row=5)
-        ttk.Button(self.modal,text="Tono... ", command=self.seleccionSonido).grid(column=1,row=5)
-        ttk.Label(self.modal,text="Posponer en").grid(column=0,row=6)
-        self.cmb_posponer = ttk.Combobox(self.modal,values=[5,10,15,20,25,30,60],justify='center',width='12',font='Arial')
-        self.cmb_posponer.current(0)
-        self.cmb_posponer.grid(column=1,row=6) #minutes
+    #ventana que notifica la alarma
+    def createNotifAlarms(self):
+        notif = tk.Tk()
+        notif.title("Ventana Emergente")
+        notif.geometry("600x300")
 
-        ttk.Button(self.modal, text="Guardar",command=self.saveAlarm).grid(column=1, row=7)
-        ttk.Button(self.modal, text="Cancelar",command=self.modal.destroy).grid(column=2,row=7)
+        #contenido o info de la alarma
 
+        ttk.Button(notif, text="Posponer",command=self.posponerAlarm).grid(column=1, row=3)
+        ttk.Button(notif, text="Ignorar",command=self.ignorarAlarm).grid(column=2,row=3)
 
-        # Bloquear interacción con la ventana principal
-        self.modal.grab_set()
+        return notif
+    #funciones de notificar la alarma
+
+    def posponerAlarm (self):
+        pass
+
+    def ignorarAlarm (self):
+        pass
+
+    def editarAlarm(self):
+        pass
